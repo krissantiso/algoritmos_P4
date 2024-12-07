d8cf071d615b3e71c6dfc2aca68c7db288b5ad7a
Kris <kris.santiso@udc.es>
2024-12-07 20:46:20 +0100

    Added nothing or smthn i dont know

--- main.c
+++ main.c
@@ -12,7 +12,7 @@
 
 
 #define DOTEST 0
-#define DOEX2 0
+#define DOEX2 1
 #define DOEX4 1
 //to reuse variables we also use this as the maximum number a node can be
 #define iterations 10000
@@ -229,7 +229,7 @@ void print_algorithms (int n, double t, int iterated, int ex, char part) {
         if (part == 'a') {
             x = t / pow(n* (log(n)), 0.8); y = t / (n* (log(n))); z = t / pow(n* (log(n)), 1.2);
         } else if (part == 'b') {
-            x = t / n; y = t / n; z = t / n;
+            x = t / pow(n, 0.8); y = t / n; z = t / pow(n, 1.2);
         } else {x = 0; y = 0; z = 0;}
     } else if (ex == 4) {
         if (part == 'a') {
@@ -333,7 +333,7 @@ double iterate_algorithms_2 (pheap h, int n, char part) {
             }
         }
         t2 = microsegundos();
-        t = t2 - t1 - repeat_rand(iterations, 1) - repeat_init();
+        t = t2 - t1 - repeat_rand(n, 0) - repeat_init();
     } else if (part == 'b') {
         int arr[n];
         t1 = microsegundos();
@@ -357,6 +357,7 @@ double iterate_algorithms_4 (int arr[], int n, char part) {
             heap_sort(arr, n, 0);
             ascending_init(arr, n);
         }
+        t2 = microsegundos();
         return t2 - t1 - repeat_asc_init(n);
     } else if (part == 'b') {
         t1 = microsegundos();
@@ -364,6 +365,7 @@ double iterate_algorithms_4 (int arr[], int n, char part) {
             heap_sort(arr, n, 0);
             descending_init(arr, n);
         }
+        t2 = microsegundos();
         return t2 - t1 - repeat_desc_init(n);
     } else if (part == 'c') {
         t1 = microsegundos();
@@ -371,6 +373,7 @@ double iterate_algorithms_4 (int arr[], int n, char part) {
             heap_sort(arr, n, 0);
             random_init(arr, n);
         }
+        t2 = microsegundos();
         return t2 - t1 - repeat_rand_init(n);
     } else return 0;
 }
@@ -395,7 +398,7 @@ double ex2_partA (pheap h, int iteration) {
         }
     }
     t2 = microsegundos();
-    return (t2 - t1);
+    return (t2 - t1) - repeat_rand(iteration, 1);
 }
 
 double ex2_partB (pheap h, int iteration) {
@@ -456,7 +459,7 @@ int main() {
     }
     if ( DOEX2 != 0) {
         print_headlines(2, 'a');
-        for (int n = 500; n <= 32000; n = n*2) {
+        for (int n = 10000; n <= 50000; n = n + 5000) {
             pheap h = malloc(sizeof(struct heap));
             init_heap(h);
             run_algorithm(h, NULL, n, 2, 'a');
